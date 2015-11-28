2695ab2a96f24d9c82e82a5aa62082e727416395
Ben Wells <mrbrwells@gmail.com>
2015-11-28 13:21:21 -0500

    building a weather app with react

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ var homeController = require('./controllers/home');
 var userController = require('./controllers/user');
 var apiController = require('./controllers/api');
 var contactController = require('./controllers/contact');
+var weatherController = require('./controllers/weather');
 
 /**
  * API keys and Passport configuration.
@@ -117,6 +118,11 @@ app.post('/account/profile', passportConf.isAuthenticated, userController.postUp
 app.post('/account/password', passportConf.isAuthenticated, userController.postUpdatePassword);
 app.post('/account/delete', passportConf.isAuthenticated, userController.postDeleteAccount);
 app.get('/account/unlink/:provider', passportConf.isAuthenticated, userController.getOauthUnlink);
+
+/**
+ * Weather App Routes
+ */
+app.get('/weather', passportConf.isAuthenticated, weatherController.weatherHome);
 
 /**
  * API examples routes.
